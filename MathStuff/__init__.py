5e006bf417e94f1edab827e6111d16aad5d9226b
Vipul Cariappa <vipulcariappa@gmail.com>
2023-01-20 17:30:37 +0530

    fix

--- MathStuff/__init__.py
+++ MathStuff/__init__.py
@@ -1,5 +1,6 @@
 from .calculus import differentiate, integrate, differentiate_latex, integrate_latex
 from .fourier_series import fourier_series_evaluator, fourier_series_latex, fourier_graph_compute, fourier_visualizer, fourier_visualizer_svg
 from .taylor_series import taylor_series, taylor_series_latex, taylor_series_computer, taylor_series_visualizer, taylor_series_visualizer_svg
+from .graph_2d import graph, graph_compute_points, graph_svg
 from .logic import truth_table_generator
 from .helper import convert_to_latex
