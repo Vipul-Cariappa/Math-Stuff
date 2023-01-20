83558d0f4b46015d6eed7b8a4e22f55375c24843
Vipul Cariappa <vipulcariappa@gmail.com>
2023-01-20 17:21:06 +0530

    updated

---
+++ MathStuff/__init__.py
@@ -0,0 +1,5 @@
+from .calculus import differentiate, integrate, differentiate_latex, integrate_latex
+from .fourier_series import fourier_series_evaluator, fourier_series_latex, fourier_graph_compute, fourier_visualizer, fourier_visualizer_svg
+from .taylor_series import taylor_series, taylor_series_latex, taylor_series_computer, taylor_series_visualizer, taylor_series_visualizer_svg
+from .logic import truth_table_generator
+from .helper import convert_to_latex
