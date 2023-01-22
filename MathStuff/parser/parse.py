4e6db9d8efe6bd1788173fb4f849b02bbcfb0cd4
Vipul Cariappa <vipulcariappa@gmail.com>
2023-01-22 15:47:05 +0530

    implemented + - * / ^

---
+++ MathStuff/parser/parse.py
@@ -0,0 +1,206 @@
+from dataclasses import dataclass
+from tokenizer import *
+
+
+@dataclass
+class IntegerNode:
+    value: int
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f"IntegerNode: {str(self.value)}"
+
+
+@dataclass
+class DecimalNode:
+    value: float
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f"DecimalNode: {str(self.value)}"
+
+
+@dataclass
+class MinusNode:
+    value: any
+
+    def __str__(self):
+        return f"(-{str(self.value)})"
+
+    def __repr__(self):
+        return f"MinusNode: {str(self)}"
+
+
+@dataclass
+class AddNode:
+    left_node: any
+    right_node: any
+
+    def __str__(self):
+        return f"({self.left_node} + {self.right_node})"
+
+    def __repr__(self) -> str:
+        return f"AddNode: {str(self)}"
+
+
+@dataclass
+class SubNode:
+    left_node: any
+    right_node: any
+
+    def __str__(self):
+        return f"({self.left_node} - {self.right_node})"
+
+    def __repr__(self) -> str:
+        return f"SubNode: {str(self)}"
+
+
+@dataclass
+class MulNode:
+    left_node: any
+    right_node: any
+
+    def __str__(self):
+        return f"({self.left_node} * {self.right_node})"
+
+    def __repr__(self) -> str:
+        return f"MulNode: {str(self)}"
+
+
+@dataclass
+class DivNode:
+    left_node: any
+    right_node: any
+
+    def __str__(self):
+        return f"({self.left_node} / {self.right_node})"
+
+    def __repr__(self) -> str:
+        return f"DivNode: {str(self)}"
+
+
+@dataclass
+class PowNode:
+    left_node: any
+    right_node: any
+
+    def __str__(self):
+        return f"({self.left_node} ^ {self.right_node})"
+
+    def __repr__(self) -> str:
+        return f"PowNode: {str(self)}"
+
+
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self._tokens_iterator = iter(tokens)
+        self._current_token = self._advance_token()
+
+    def _advance_token(self):
+        try:
+            return next(self._tokens_iterator)
+        except StopIteration:
+            return None
+
+    def parse(self):
+        result = self._create_expression()
+
+        if self._current_token is None:
+            return result
+
+        raise SyntaxError(f"Did not expect {self._current_token} after {result}")
+
+    def _create_expression(self):
+        if self._current_token is None:
+            return None
+
+        lhs = self._create_term()
+
+        if self._current_token is AddToken or self._current_token is SubToken:
+            token = self._current_token
+            self._current_token = self._advance_token()
+            rhs = self._create_expression()
+
+            if token is AddToken:
+                return AddNode(lhs, rhs)
+
+            return SubNode(lhs, rhs)
+
+        elif self._current_token is None:
+            return lhs
+
+        raise SyntaxError(
+            f"Expented AddToken or SubToken here but got {self._current_token}"
+        )
+
+    def _create_term(self):
+        if self._current_token is None:
+            return None
+
+        lhs = self._create_power_term()
+
+        if self._current_token is MulToken or self._current_token is DivToken:
+            token = self._current_token
+            self._current_token = self._advance_token()
+            rhs = self._create_term()
+
+            if token is MulToken:
+                return MulNode(lhs, rhs)
+
+            return DivNode(lhs, rhs)
+
+        return lhs
+
+    def _create_power_term(self):
+        if self._current_token is None:
+            return None
+
+        lhs = self._create_factor()
+
+        if self._current_token is PowToken:
+            token = self._current_token
+            self._current_token = self._advance_token()
+            rhs = self._create_power_term()
+
+            return PowNode(lhs, rhs)
+
+        return lhs
+
+    def _create_factor(self):
+        if self._current_token is None:
+            return None
+
+        if self._current_token is AddToken or self._current_token is SubToken:
+            token = self._current_token
+            self._current_token = self._advance_token()
+
+            factor = self._create_factor()
+
+            if token is SubToken:
+                return MinusNode(factor)
+
+        if isinstance(self._current_token, DecimalToken):
+            token = self._current_token
+            self._current_token = self._advance_token()
+            return DecimalNode(token.value)
+
+        elif isinstance(self._current_token, IntegerToken):
+            token = self._current_token
+            self._current_token = self._advance_token()
+            return IntegerNode(token.value)
+
+        else:
+            raise SyntaxError(
+                f"Got unexpected token {self._current_token} to Parse._create_factor"
+            )
+
+
+if __name__ == "__main__":
+    from tokenizer import Tokenizer
+
+    print(Parser(Tokenizer(input("math > ")).generate_tokens()).parse())
---
+++ MathStuff/parser/tokenizer.py
@@ -0,0 +1,162 @@
+from dataclasses import dataclass
+
+
+DIGITS = "0123456789"
+IGNORE = " \n\t\r"
+
+
+@dataclass
+class IntegerToken:
+    value: int
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self) -> str:
+        return f"IntegerToken: {self.value}"
+
+
+@dataclass
+class DecimalToken:
+    value: float
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self) -> str:
+        return f"DecimalToken: {self.value}"
+
+
+@dataclass
+class AddToken:
+    def __str__(self):
+        return "+"
+
+    def __repr__(self) -> str:
+        return "AddToken: +"
+
+
+@dataclass
+class SubToken:
+    def __str__(self):
+        return "-"
+
+    def __repr__(self) -> str:
+        return "SubToken: -"
+
+
+@dataclass
+class MulToken:
+    def __str__(self):
+        return "*"
+
+    def __repr__(self) -> str:
+        return "MulToken: *"
+
+
+@dataclass
+class DivToken:
+    def __str__(self):
+        return "/"
+
+    def __repr__(self) -> str:
+        return "DivToken: /"
+
+
+@dataclass
+class PowToken:
+    def __str__(self):
+        return "^"
+
+    def __repr__(self) -> str:
+        return "PowToken: ^"
+
+
+@dataclass
+class OPToken:
+    def __str__(self):
+        return "("
+
+    def __repr__(self) -> str:
+        return "OPToken: ("
+
+
+@dataclass
+class CPToken:
+    def __str__(self):
+        return ")"
+
+    def __repr__(self) -> str:
+        return "CPToken: )"
+
+
+# TODO: add the following tokens: | & ~
+
+
+class Tokenizer:
+    def __init__(self, string: str):
+        self.string = string
+        self._string_iterator = iter(self.string)
+        self._current_character = self._advance_string()
+
+    def _advance_string(self):
+        try:
+            return next(self._string_iterator)
+        except StopIteration:
+            return None
+
+    def generate_tokens(self):
+        while self._current_character is not None:
+            if self._current_character in IGNORE:
+                pass
+            elif self._current_character == "+":
+                yield AddToken
+            elif self._current_character == "-":
+                yield SubToken
+            elif self._current_character == "*":
+                yield MulToken
+            elif self._current_character == "/":
+                yield DivToken
+            elif self._current_character == "^":
+                yield PowToken
+            elif self._current_character == "(":
+                yield OPToken
+            elif self._current_character == ")":
+                yield CPToken
+            elif self._current_character in DIGITS or self._current_character == ".":
+                yield self._generate_number()
+            else:
+                raise SyntaxError(
+                    f"Could not tokenize the input text: {self._current_character}"
+                )
+
+            self._current_character = self._advance_string()
+
+    def _generate_number(self):
+        is_decimal = False
+        number = ""
+
+        while self._current_character is not None:
+            if self._current_character in DIGITS:
+                number += self._current_character
+
+            elif self._current_character == ".":
+                if is_decimal is True:
+                    raise SyntaxError("Found 2 decimal points in the number")
+
+                is_decimal = True
+                number = self._current_character if number != "" else "0."
+            else:
+                if is_decimal:
+                    return DecimalToken(float(number))
+                return IntegerToken(int(number))
+
+            self._current_character = self._advance_string()
+
+        if is_decimal:
+            return DecimalToken(float(number))
+        return IntegerToken(int(number))
+
+
+if __name__ == "__main__":
+    print(list(Tokenizer(input("math > ")).generate_tokens()))
