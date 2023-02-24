8d9680ae90eb4d6b1ac342b2c9ced61dfabe9082
ravikamath123 <ravivkamath@gmail.com>
2023-02-24 20:46:01 +0530

    Have added test_combinations for 1,2,4,5 variables and evaluated complex equation in 4,5 variables.

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -3,6 +3,8 @@ from MathStuff import logic
 a = logic.Variable("a")
 b = logic.Variable("b")
 c = logic.Variable("c")
+d = logic.Variable("d")
+e = logic.Variable("e")
 
 
 def evaluator2(tb, equation_str, result_str):
@@ -34,6 +36,34 @@ def evaluator3(tb, equation_str, result_str):
         assert tb[result_str][i] == eval(equation_str)
 
 
+def evaluator4(tb, equation_str, result_str):
+    """
+    Works for equation with four variables
+    """
+
+    for i in range(16):
+        a = tb["a"][i]
+        b = tb["b"][i]
+        c = tb["c"][i]
+        d = tb["d"][i]
+
+        assert tb[result_str][i] == eval(equation_str)
+
+
+def evaluator5(tb, equation_str, result_str):
+    """
+    Works for equation wiht five variables
+    """
+    for i in range(32):
+        a = tb["a"][i]
+        b = tb["b"][i]
+        c = tb["c"][i]
+        d = tb["d"][i]
+        e = tb["e"][i]
+
+        assert tb[result_str][i] == eval(equation_str)
+
+
 def test_negation():
     """
     Truth Table
@@ -42,6 +72,7 @@ def test_negation():
     ----------
     | 0 |  1 |
     | 1 |  0 |
+    ----------
     """
 
     eq = ~a
@@ -63,6 +94,7 @@ def test_or():
     | 1 | 0 |   1   |
     | 0 | 1 |   1   |
     | 1 | 1 |   1   |
+    -----------------
     """
 
     eq = a + b
@@ -81,6 +113,7 @@ def test_and():
     | 1 | 0 |   0   |
     | 0 | 1 |   0   |
     | 1 | 1 |   1   |
+    -----------------
     """
 
     eq = a * b
@@ -99,6 +132,7 @@ def test_conditional():
     | 1 | 0 |   0    |
     | 0 | 1 |   1    |
     | 1 | 1 |   1    |
+    ------------------
     """
 
     eq = a / b
@@ -117,6 +151,7 @@ def test_biconditional():
     | 1 | 0 |   0   |
     | 0 | 1 |   0   |
     | 1 | 1 |   1   |
+    -----------------
     """
 
     eq = a % b
@@ -135,6 +170,7 @@ def test_xor():
     | 1 | 0 |   1   |
     | 0 | 1 |   1   |
     | 1 | 1 |   0   |
+    -----------------
     """
 
     eq = a ^ b
@@ -157,6 +193,7 @@ def test_complex1():
     | 1 | 0 | 1 |      1       |
     | 0 | 1 | 1 |      1       |
     | 1 | 1 | 1 |      1       |
+    ----------------------------
     """
 
     eq = a + b * c
@@ -175,6 +212,7 @@ def test_complex2():
     | 1 | 0 |       0       |
     | 0 | 1 |       1       |
     | 1 | 1 |       1       |
+    -------------------------
     """
 
     eq = (a + (~b)) / b
@@ -183,7 +221,123 @@ def test_complex2():
     evaluator2(table, "int(not(a or (not(b))) or b)", "( a + ( ~ b ) ) -> b")
 
 
-def test_combinations():
+def test_complex3():
+    """
+    Truth Table
+
+    | a | b | c | d | (a + c) * (b + d) |
+    -------------------------------------
+    | 0 | 0 | 0 | 0 |       0           |
+    | 0 | 0 | 0 | 1 |       0           |
+    | 0 | 0 | 1 | 0 |       0           |
+    | 0 | 0 | 1 | 1 |       1           |
+    | 0 | 1 | 0 | 0 |       0           |
+    | 0 | 1 | 0 | 1 |       0           |
+    | 0 | 1 | 1 | 0 |       1           |
+    | 0 | 1 | 1 | 1 |       1           |
+    | 1 | 0 | 0 | 0 |       0           |
+    | 1 | 0 | 0 | 1 |       1           |
+    | 1 | 0 | 1 | 0 |       0           |
+    | 1 | 0 | 1 | 1 |       1           |
+    | 1 | 1 | 0 | 0 |       1           |
+    | 1 | 1 | 0 | 1 |       1           |
+    | 1 | 1 | 1 | 0 |       1           |
+    | 1 | 1 | 1 | 1 |       1           |
+    -------------------------------------
+    """
+    eq = (a + c) * (b + d)
+    tb = eq.generate_truth_table()
+
+    evaluator4(tb, "int((a or c) and (b or d))", "( a + c ) * ( b + d )")
+
+
+def test_complex4():
+    """
+    Truth Table
+
+    | a | b | c | d | e | (a + b + ~e) * (c + d) |
+    ----------------------------------------------
+    | 0 | 0 | 0 | 0 | 0 |           0            |
+    | 0 | 0 | 0 | 0 | 1 |           0            |
+    | 0 | 0 | 0 | 1 | 0 |           1            |
+    | 0 | 0 | 0 | 1 | 1 |           0            |
+    | 0 | 0 | 1 | 0 | 0 |           1            |
+    | 0 | 0 | 1 | 0 | 1 |           0            |
+    | 0 | 0 | 1 | 1 | 0 |           1            |
+    | 0 | 0 | 1 | 1 | 1 |           0            |
+    | 0 | 1 | 0 | 0 | 0 |           0            |
+    | 0 | 1 | 0 | 0 | 1 |           0            |
+    | 0 | 1 | 0 | 1 | 0 |           1            |
+    | 0 | 1 | 0 | 1 | 1 |           1            |
+    | 0 | 1 | 1 | 0 | 0 |           1            |
+    | 0 | 1 | 1 | 0 | 1 |           1            |
+    | 0 | 1 | 1 | 1 | 0 |           1            |
+    | 0 | 1 | 1 | 1 | 1 |           1            |
+    | 1 | 0 | 0 | 0 | 0 |           0            |
+    | 1 | 0 | 0 | 0 | 1 |           0            |
+    | 1 | 0 | 0 | 1 | 0 |           1            |
+    | 1 | 0 | 0 | 1 | 1 |           1            |
+    | 1 | 0 | 1 | 0 | 0 |           1            |
+    | 1 | 0 | 1 | 0 | 1 |           1            |
+    | 1 | 0 | 1 | 1 | 0 |           1            |
+    | 1 | 0 | 1 | 1 | 1 |           1            |
+    | 1 | 1 | 0 | 0 | 0 |           0            |
+    | 1 | 1 | 0 | 0 | 1 |           0            |
+    | 1 | 1 | 0 | 1 | 0 |           1            |
+    | 1 | 1 | 0 | 1 | 1 |           1            |
+    | 1 | 1 | 1 | 0 | 0 |           1            |
+    | 1 | 1 | 1 | 0 | 1 |           1            |
+    | 1 | 1 | 1 | 1 | 0 |           1            |
+    | 1 | 1 | 1 | 1 | 1 |           1            |
+    ----------------------------------------------
+    """
+
+    eq = (a + b + (~e)) * (c + d)
+    table = eq.generate_truth_table()
+
+    evaluator5(
+        table,
+        "int((a or b or (not(e))) and (c or d))",
+        "( ( a + b ) + ( ~ e ) ) * ( c + d )",
+    )
+
+
+def test_combinations1():
+    """
+    Check uniqueness of each combination in one variable combination
+    """
+    eq = ~a
+    tb = eq.generate_truth_table()
+
+    for i in range(2):
+        an = tb["a"][i]
+
+        number = an
+
+        assert number == i
+
+
+def test_combinations2():
+    """
+    Check uniqueness of each combination in two variable combination
+    """
+    eq = a + b
+    tb = eq.generate_truth_table()
+
+    for i in range(4):
+        an = tb["a"][i]
+        bn = tb["b"][i]
+
+        number = (an << 1) + bn
+
+        assert number == i
+
+
+def test_combinations3():
+    """
+    Check uniqueness of each combination in three variable combination
+    """
+
     eq = a + b + c
     tb = eq.generate_truth_table()
 
@@ -195,4 +349,40 @@ def test_combinations():
         number = (an << 2) + (bn << 1) + cn
 
         assert number == i
-        
+
+
+def test_combinations4():
+    """
+    Check uniqueness of each combination in four variable combination
+    """
+    eq = a + b + c + d
+    table = eq.generate_truth_table()
+
+    for i in range(16):
+        an = table["a"][i]
+        bn = table["b"][i]
+        cn = table["c"][i]
+        dn = table["d"][i]
+
+        number = (an << 3) + (bn << 2) + (cn << 1) + dn
+
+        assert number == i
+
+
+def test_combinations5():
+    """
+    Check uniqueness of each combination in five variable combination
+    """
+    eq = a + b + c + d + e
+    tb = eq.generate_truth_table()
+
+    for i in range(32):
+        an = tb["a"][i]
+        bn = tb["b"][i]
+        cn = tb["c"][i]
+        dn = tb["d"][i]
+        en = tb["e"][i]
+
+        number = (an << 5) + (bn << 3) + (cn << 2) + (dn << 1) + en
+
+        assert number == i
